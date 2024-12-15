9931e1acba01974c9ed8530c7e3099184dd74fca
SIRIGIRI VAMSI KRISHNA <146564008+vamsi200414@users.noreply.github.com>
2024-12-15 17:50:19 +0530

    Arrange Alphabets in Descending order.java

--- Java Codes/Day1/Arrange Alphabets in Descending order.java
+++ Java Codes/Day1/Arrange Alphabets in Descending order.java
@@ -1,1 +1,30 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner abc = new Scanner(System.in);
+		String s = abc.nextLine();
+		StringBuilder sb = new StringBuilder();
+		boolean[] seen = new boolean[256];
+		for(int i=0 ; i<s.length() ; i++){
+		    char ch = s.charAt(i);
+		    if(!seen[ch]){
+		        seen[ch] = true;
+		        sb.append(ch);
 
+		    }
+		}
+		char[] c = sb.toString().toCharArray();
+		for(int i=0 ; i<c.length-1 ; i++){
+		    for(int  j=i+1 ; j<c.length ; j++){
+		        if(c[i] < c[j]){
+		            char temp = c[i];
+		            c[i] = c[j];
+		            c[j] = temp;
+		        }
+		    }
+		}
+		String result = new  String(c);
+		System.out.print(result);
+	}
+}
