e9e7886361077c6ff862a5b4fe81b2bff9e311cb
SIRIGIRI VAMSI KRISHNA <146564008+vamsi200414@users.noreply.github.com>
2025-02-08 10:23:42 +0530

    Create Largest of 3 numbers.py

---
+++ python codes/Largest of 3 numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+c = int(input("Enter 3rd number : "))
+if(a>b and a>c):
+    print(a,"is greater")
+elif(b>c and b>a):
+    print(b,"is greater")
+else:
+    print(c,"is greater")
